2b5e373b1382926b01e1d3dc2423ce6aa96d2a11
rohit1631007 <59388222+rohit1631007@users.noreply.github.com>
2025-05-20 19:02:21 +0000

    changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const   accountId=1234567
+let accountName="Rohit jaiswal"
+var accountMail="tryei@gmail.com"
+accountCity="Bengaluru"
+
+console.log(accountMail,accountId,accountName,accountCity);
+
+
+
+//Prefer not to use var because it have issue with block scope and functional Scope.//
+//var	Function-scoped
+//let	Block-scoped ({})
+//const	Block-scoped ({})
+//var is visible throughout the function in which it is declared.
+
+//let and const are only visible within the block {} where they are declared.
+
